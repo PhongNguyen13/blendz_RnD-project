f7fb68e1af2d26e9956069cfca9ad7e843403461
Jason-5-37 <xrf4650@aut.ac.nz>
2022-02-24 22:27:46 +1300

    fix the app.js

---
+++ rnd-blendz/src/Pages/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Shop extends React.Component{
+    render(){
+        return(
+        <div>
+            <div>This is Shop!</div>
+            </div>
+            );
+        }
+    }
+ 
+export default Shop;
