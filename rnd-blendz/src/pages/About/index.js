c5a95fd7e4b50d1c6a9da4c32893dc27e73bdd21
PhongNguyen13 <81122421+PhongNguyen13@users.noreply.github.com>
2022-03-04 14:09:22 +1300

    Changes to About page

--- rnd-blendz/src/pages/About/index.js
+++ rnd-blendz/src/pages/About/index.js
@@ -7,13 +7,11 @@ class About extends Component {
             <div className="AboutWrapper">
                 <div className="InfoWrapper">
                     <h1 className="title">We are Blendz</h1>
-                    <p className="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown prin
-                    ter took a galley of type and scrambled it to make a type specimen book. It has survived not only
-                    five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-                    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem 
-                    Ipsum.</p>
+                    <p className="content">Blendz is the New Zealand and South Pacific distributor of slushy machines from Elmeco in Italy.<br />
+              We offer machine sales and support, rentals, servicing, spare parts, slushy concentrate and accessories as a complete hassle free solution.<br />
+              The ideas for Blendz are constantly being developed and tested to provide a range of unique and interesting beverages.<br />
+              '2 Mix it' was conceived in conjunction with Blendz, as the vehicle in which the blended beverages can be administered.<br />
+              On this site you will see our dispensing machinery and accessories which allows you to make this happen for your business.</p>
                 </div>
             </div>
         )
