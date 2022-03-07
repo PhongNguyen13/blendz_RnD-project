27bb5f84ad45c25281a65279b294e82df55012d6
Jason-5-37 <xrf4650@aut.ac.nz>
2022-03-08 09:47:00 +1300

    fixing help page

--- rnd-blendz/src/pages/Help/index.js
+++ rnd-blendz/src/pages/Help/index.js
@@ -4,17 +4,22 @@ import './help.css';
 class Help extends Component {
     render(){
         return(
-            <body>
             <div class="container-fluid">
             <h2>Frequently Asked Questions (FAQs)</h2>
             <div class="accordion">
                 <div class="icon"></div>
                 <h5>Question 1?</h5> 
             </div>
-
+            
+            {this.getAnswer}
             <div class="panel">
+            {this.getAnswer}
                 <p>Test answer 1..............</p>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/ZNMeyrgmr5o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/ZNMeyrgmr5o" 
+                title="YouTube video player" 
+                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen></iframe>
+                {this.getAnswer}
             </div>
 
             <div class="accordion">
@@ -47,33 +52,34 @@ class Help extends Component {
             <div class="accordion">
                 <div class="icon"></div>
                 <h5>Question 5?</h5> 
+                {this.getAnswer}
             </div>
 
             <div class="panel">
                 <p>Test answer 5...............</p>
             </div>
+            {this.getAnswer}
         </div>
-        </body>
         )
+    }    
+
+    getAnswer() {
+            var acc = document.getElementsByClassName('accordion');
+            var i;
+            var len = acc.length;
+
+            for(i = 0; i < len; i++) {
+                acc[i].addEventListener('click', function() {
+                this.classList.toggle('active');
+                var panel = this.nextElementSibling;
+                if(panel.style.maxHeight) {
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight + 'px';
+                }
+            })
     }
 }
-
-const displayAnswer = () => ({
-    let acc = document.getElementsByClassName('accordion');
-    var i;
-    var len = acc.length;
-
-for(i = 0; i < len; i++) {
-    acc[i].addEventListener('click', function() {
-        this.classList.toggle('active');
-        var panel = this.nextElementSibling;
-        if(panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + 'px';
-        }
-    })
 }
-});
 
-export default connect(displayAnswer)(Help);
+export default Help;
