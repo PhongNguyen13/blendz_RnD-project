ac5d76b24de689d2e0a550043d715f90e97eace7
Jason-5-37 <xrf4650@autuni.ac.nz>
2022-03-08 14:33:48 +1300

    update the help page

--- rnd-blendz/src/pages/Help/index.js
+++ rnd-blendz/src/pages/Help/index.js
@@ -1,85 +1,86 @@
 import React, { Component } from "react";
-import './help.css';
+import { Answer, Button, HelpWrapper, Question, QuestionWrapper } from "./style";
+import { connect } from 'react-redux';
+import { actionCreators } from './store';
 
 class Help extends Component {
     render(){
         return(
+            <div>
+                <div><br><br></br></br></div>
+            <nav class="navbar"></nav>
+     
             <div class="container-fluid">
-            <h2>Frequently Asked Questions (FAQs)</h2>
-            <div class="accordion">
-                <div class="icon"></div>
-                <h5>Question 1?</h5> 
-            </div>
-            
-            {this.getAnswer}
-            <div class="panel">
-            {this.getAnswer}
-                <p>Test answer 1..............</p>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/ZNMeyrgmr5o" 
-                title="YouTube video player" 
-                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                allowfullscreen></iframe>
-                {this.getAnswer}
-            </div>
-
-            <div class="accordion">
-                <div class="icon"></div>
-                <h5>Question 2?</h5> 
-            </div>
-
-            <div class="panel">
-                <p>Test answer 2...............</p>
-            </div>
-
-            <div class="accordion">
-                <div class="icon"></div>
-                <h5>Question 3?</h5> 
-            </div>
-
-            <div class="panel">
-                <p>Test answer 3...............</p>
-            </div>
-
-            <div class="accordion">
-                <div class="icon"></div>
-                <h5>Question 4?</h5> 
-            </div>
-
-            <div class="panel">
-                <p>Test answer 4...............</p>
-            </div>
-
-            <div class="accordion">
-                <div class="icon"></div>
-                <h5>Question 5?</h5> 
-                {this.getAnswer}
-            </div>
-
-            <div class="panel">
-                <p>Test answer 5...............</p>
-            </div>
-            {this.getAnswer}
-        </div>
+                <h2>Frequently Asked Questions (FAQs)</h2>
+                <div class="accordion">
+                    <div class="icon"></div>
+                    <h5>Question 1?</h5>
+                    {getQuestion()}
+                </div>
+    
+                <div class="panel">
+                    <p>Test answer 1..............</p>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/ZNMeyrgmr5o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+    
+                <div class="accordion">
+                    <div class="icon"></div>
+                    <h5>Question 2?</h5> 
+                </div>
+    
+                <div class="panel">
+                    <p>Test answer 2...............</p>
+                </div>
+    
+                <div class="accordion">
+                    <div class="icon"></div>
+                    <h5>Question 3?</h5> 
+                </div>
+    
+                <div class="panel">
+                    <p>Test answer 3...............</p>
+                </div>
+    
+                <div class="accordion">
+                    <div class="icon"></div>
+                    <h5>Question 4?</h5> 
+                </div>
+    
+                <div class="panel">
+                    <p>Test answer 4...............</p>
+                </div>
+    
+                <div class="accordion">
+                    <div class="icon"></div>
+                    <h5>Question 5?</h5> 
+                </div>
+    
+                <div class="panel">
+                    <p>Test answer 5...............</p>
+                </div>
+            </div></div>
         )
-    }    
+    }
 
-    getAnswer() {
-            var acc = document.getElementsByClassName('accordion');
-            var i;
-            var len = acc.length;
+    getQuestion(){
+        
+var acc = document.getElementsByClassName('accordion');
+var i;
+var len = acc.length;
 
-            for(i = 0; i < len; i++) {
-                acc[i].addEventListener('click', function() {
-                this.classList.toggle('active');
-                var panel = this.nextElementSibling;
-                if(panel.style.maxHeight) {
-                    panel.style.maxHeight = null;
-                } else {
-                    panel.style.maxHeight = panel.scrollHeight + 'px';
-                }
-            })
+for(i = 0; i < len; i++) {
+    acc[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+        var panel = this.nextElementSibling;
+        if(panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+    })
+}
     }
 }
-}
+
 
 export default Help;
