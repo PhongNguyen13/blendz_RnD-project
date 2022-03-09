b44fbd6e19153befeca15428d968edbe2cd450bc
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2022-03-10 12:34:27 +1300

    Copy Jason2

---
+++ rnd-blendz/src/store/reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux-immutable';
+import { reducer as headerReducer } from '../common/header/store';
+import { reducer as loginReducer } from '../pages/login/store/all.js';
+
+const reducer = combineReducers({
+    header: headerReducer,
+    login: loginReducer
+})
+
+export default reducer;
