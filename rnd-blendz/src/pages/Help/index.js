8f9c4c43c324583c53e1a3aaddb7ee53d836e921
PhongNguyen13 <81122421+PhongNguyen13@users.noreply.github.com>
2022-03-12 11:09:50 +1300

    Commit

--- rnd-blendz/src/pages/Help/index.js
+++ rnd-blendz/src/pages/Help/index.js
@@ -1,80 +1,68 @@
+//import { get } from "immutable";
 import React, { Component } from "react";
-import './help.css'
+import './help.css';
+import { useState } from "react";
 
-class Help extends Component {
-    render(){
+function Help() {
+    //render(){
+        /*function getQuestion() {
+            var acc = document.getElementsByClassName('accordion');
+            var i;
+            var len = acc.length;
+        
+            for(i = 0; i < len; i++) {
+                acc[i].addEventListener('click', function() {
+                    this.classList.toggle('active');
+                    var panel = this.nextElementSibling;
+                    if(panel.style.maxHeight) {
+                        panel.style.maxHeight = null;
+                    } else {
+                        panel.style.maxHeight = panel.scrollHeight + 'px';
+                    }
+                })
+            }
+        }*/ 
+
+        const data = [
+            {
+                question: 'Question 1',
+                answer: 'Test answer 1 ....',
+            },
+            {
+                question: 'Question 2',
+                answer: 'Test answer 2 ....',
+            }
+        ];
+
+        
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const [selected, setSelected] = useState(null);
+
+        const toggle = (i) => {
+            if (selected === i) {
+                return setSelected(null);
+            }
+
+            setSelected(i);
+        };
+
         return(
-            <div>
-            <nav class="navbar"></nav>
-     
-            <div class="container-fluid">
-                <h2>Frequently Asked Questions (FAQs)</h2>
-                <div class="accordion">
-                    <div class="icon"></div>
-                    <h5>Question 1?</h5>
+            
+            <div class='wrapper'>
+                <div class='accordion'>
+                    {data.map((item, i) => (
+                        <div class='item'>
+                            <div class='title' onClick={() => toggle(i)}>
+                                <h2>{item.question}</h2>
+                                <span>{selected === i ? '-' : '+'}</span>
+                            </div>
+                            <div class={selected === i ? 'show' : 'content'}>{item.answer}</div>
+                        </div>   
+                    ))}
                 </div>
-    
-                <div class="panel">
-                    <p>Test answer 1..............</p>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/ZNMeyrgmr5o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-    
-                <div class="accordion">
-                    <div class="icon"></div>
-                    <h5>Question 2?</h5> 
-                </div>
-    
-                <div class="panel">
-                    <p>Test answer 2...............</p>
-                </div>
-    
-                <div class="accordion">
-                    <div class="icon"></div>
-                    <h5>Question 3?</h5> 
-                </div>
-    
-                <div class="panel">
-                    <p>Test answer 3...............</p>
-                </div>
-    
-                <div class="accordion">
-                    <div class="icon"></div>
-                    <h5>Question 4?</h5> 
-                </div>
-    
-                <div class="panel">
-                    <p>Test answer 4...............</p>
-                </div>
-    
-                <div class="accordion">
-                    <div class="icon"></div>
-                    <h5>Question 5?</h5> 
-                </div>
-    
-                <div class="panel">
-                    <p>Test answer 5...............</p>
-                </div>
-            </div></div>
+            </div>
         )
-    }
-
-    getQuestion() {
-        var acc = document.getElementsByClassName('accordion');
-        var i;
-        var len = acc.length;
-    
-        for(i = 0; i < len; i++) {
-            acc[i].addEventListener('click', function() {
-                this.classList.toggle('active');
-                var panel = this.nextElementSibling;
-                if(panel.style.maxHeight) {
-                    panel.style.maxHeight = null;
-                } else {
-                    panel.style.maxHeight = panel.scrollHeight + 'px';
-                }
-            })
-        }
-    }
-  
+    //}
 }
+
 export default Help;
