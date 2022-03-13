71803dfe11dc2be43ce422c0d9cc8c129413414c
PhongNguyen13 <81122421+PhongNguyen13@users.noreply.github.com>
2022-03-13 20:57:46 +1300

    Fix Help page

--- rnd-blendz/src/pages/Help/Data.js
+++ rnd-blendz/src/pages/Help/Data.js
@@ -1,14 +1,26 @@
 export const Data = [
     {
-      question: 'What do you call a dog magician?',
-      answer: 'A labracadabrador.'
+      question: 'My First Class 1 machine is not cooling?',
+      answer: 'The machine has overheated and requires the reset button to be pushed.'
     },
     {
-      question: 'What do you call a funny mountain?',
-      answer: 'Hill-arious.'
+      question: 'How does a slush machine work?',
+      answer: 'The machine is operating at -3 degrees Celsius, by having a product with a minimum of 14% sugar and continuous agitation the machine will shave the loosely bonded ice crystals to form a slush.'
     },
     {
-      question: 'What did the astronaut say when he crashed into the moon?',
-      answer: 'I Apollo-gize.'
+      question: 'Can I put ordinary soft drinks into my slush machine?',
+      answer: 'No. It is not recommended to use any product other than one that is especially formulated for the machines.'
+    },
+    {
+      question: 'Can I put alcohol into my machine?',
+      answer: 'Yes. Alcohol does not freeze so you can add it to any slush product without upsetting the mix ratio (usually 4:1).'
+    },
+    {
+      question: 'My Bigbiz 2 machine is not chilling, all the lights are flashing?',
+      answer: 'The machine has overheated and popped the cutout.</br>The reset button is on the front of the machine behind the left hand side drip tray.'
+    },
+    {
+      question: 'First Class 2 or 3 not freezing?',
+      answer: 'Requires the reset button pushed.'
     }
   ];
--- rnd-blendz/src/pages/Help/index.js
+++ rnd-blendz/src/pages/Help/index.js
@@ -5,38 +5,50 @@ import { IconContext } from 'react-icons';
 import { FiPlus, FiMinus } from 'react-icons/fi';
 
 const AccordionSection = styled.div`
-  display: flex;
+  margin-top: 150px;
+  margin bottom: 150px;
+  margin-left: 180px;
+  height: 50vh;
+  width: 100%;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   position: relative;
-  height: 50vh;
-  width: 100%
   background: #fff;
+
+  &::-webkit-scrollbar {
+    width: 10px;
+    border: 1px solid black;
+  }
 
   h1 {
     color: hotpink;
     position: relative;
     width: max-width;
-    font-size: 30px;
+    font-size: 35px;
+  }
+
+  h1::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    margin-right: 200px;
+    width: 310px;
+    height: 2px;
+    background-color: hotpink;
   }
 `;
 
 /*const Container = styled.div`
   position: absolute;
-  top: 40%;
-  box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3);
-`;*/
-const Container = styled.div`
-  position: absolute;
   top: 60%;
-`;
+`;*/
 
 const Wrap = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 100%;
+  width: 70%;
   text-align: center;
   cursor: pointer;
   border: 2px solid #6db5ff;
@@ -45,7 +57,7 @@ const Wrap = styled.div`
   h2 {
     color: #1f5c9a;
     padding: 5px 10px 10px 10px;
-    font-size: 22px;
+    font-size: 17px;
     font-weight: normal;
     transition: all 0.5s ease-in;
   }
@@ -63,15 +75,14 @@ const Wrap = styled.div`
 const Dropdown = styled.div`
   background-color: #6db5ff;
   color: #fff;
-  width: 100%;
+  width: 70%;
   display: flex;
-  flex-direction: column;
   border-left: 2px solid #fff;
   border-radius: 50px;
   p {
     padding: 5px 10px 10px 10px;
     margin-left: 25px;
-    font-size: 20px;
+    font-size: 15px;
     text-align: justify;
   }
 `;
@@ -92,11 +103,9 @@ const Help = () => {
     <IconContext.Provider value={{ color: '#fff', size: '25px' }}>
       <AccordionSection>
       <h1>FREQUENTLY ASKED QUESTION</h1>
-        <Container>
           {Data.map((item, index) => {
             return (
               <>
-                
                 <Wrap onClick={() => toggle(index)} key={index}>
                   <h2>{item.question}</h2>
                   <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
@@ -109,7 +118,6 @@ const Help = () => {
               </>
             );
           })}
-        </Container>
       </AccordionSection>
     </IconContext.Provider>
   );
