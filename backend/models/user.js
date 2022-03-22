3579736d483da324d7a2650707fd62e8e41dbffd
Jason-5-37 <xrf4650@aut.ac.nz>
2022-03-22 14:13:12 +1300

    create the backend & connect to firebase

---
+++ backend/models/user.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(firstName, lastName, Email, Password, PhoneNumber ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.Email = Email;
+        this.Password = Password;
+        this.PhoneNumber = PhoneNumber;
+    }
+}
+
+module.exports = User;
