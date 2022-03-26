7af72f4abbb88327bbaf323e7c54a2869793cfc0
PhongNguyen13 <81122421+PhongNguyen13@users.noreply.github.com>
2022-03-26 15:43:49 +1300

    Help changed

--- rnd-blendz/src/pages/Help/Data.js
+++ rnd-blendz/src/pages/Help/Data.js
@@ -1,14 +1,30 @@
 export const Data = [
-    {
-      question: 'What do you call a dog magician?',
-      answer: 'A labracadabrador.'
-    },
-    {
-      question: 'What do you call a funny mountain?',
-      answer: 'Hill-arious.'
-    },
-    {
-      question: 'What did the astronaut say when he crashed into the moon?',
-      answer: 'I Apollo-gize.'
-    }
-  ];
+  {
+    question: 'My First Class 1 machine is not cooling?',
+    answer: 'The machine has overheated and requires the reset button to be pushed.',
+    link: <iframe src="https://drive.google.com/file/d/1C-uoAYQ0Edy7lflZI4jqwuXGIY3PklJ5/preview" title="Answer PDF" width="640" height="480" allow="autoplay"></iframe>
+  },
+  {
+    question: 'How does a slush machine work?',
+    answer: "The machine is operating at -3 degrees Celsius, by having a product with a minimum of 14% sugar and continuous agitation the machine will shave the loosely bonded ice crystals to form a slush.",
+    link: <iframe width="560" height="315" src="https://www.youtube.com/embed/UgfACPnSziI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  },
+  {
+    question: 'Can I put ordinary soft drinks into my slush machine?',
+    answer: 'No. It is not recommended to use any product other than one that is especially formulated for the machines.'
+  },
+  {
+    question: 'Can I put alcohol into my machine?',
+    answer: 'Yes. Alcohol does not freeze so you can add it to any slush product without upsetting the mix ratio (usually 4:1).'
+  },
+  {
+    question: 'My Bigbiz 2 machine is not chilling, all the lights are flashing?',
+    answer: 'The machine has overheated and popped the cutout.</br>The reset button is on the front of the machine behind the left hand side drip tray.',
+    link: <iframe src="https://drive.google.com/file/d/1iWLmgaMsQWinD6TwU4rHpdhW19uF2ZDZ/preview" title = "Answer PDF" width="640" height="480" allow="autoplay"></iframe>,
+    link2: <iframe src="https://drive.google.com/file/d/1g0s1EhFH7oS5Z4iK_ttKws3DfhEjMqNz/preview" title="Answer" width="640" height="480" allow="autoplay"></iframe>
+  },
+  {
+    question: 'First Class 2 or 3 not freezing?',
+    answer: 'Requires the reset button pushed.'
+  }
+];
