0754d3c45c100fcdd212e1a41260770b6a4a0d1e
Jason-5-37 <xrf4650@aut.ac.nz>
2022-03-29 20:14:45 +1300

    'd'

--- frontend/src/pages/login/store/constants.js
+++
@@ -1,4 +0,0 @@
-export const CHANGE_LOGIN = 'login/CHANGE_LOGIN';
-export const CHANGE_LOGOUT = 'login/CHANGE_LOGOUT';
-export const CHANGE_RLOGIN = 'login/CHANGE_RLOGIN';
-export const GET_USER = 'login/GET_USER';
