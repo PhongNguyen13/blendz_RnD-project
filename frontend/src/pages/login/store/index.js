ab208febec9a0e78e3bebc3f8b0f5dd7b0888108
Jason-5-37 <xrf4650@aut.ac.nz>
2022-03-29 20:06:56 +1300

    'd'

--- frontend/src/pages/login/store/index.js
+++
@@ -1,6 +0,0 @@
-import reducer from "./reducer";
-import * as actionCreators from './actionCreators';
-import * as constants from './constants';
-import * as actionCreators2 from '../../Register/store/actionCreators';
-
-export { reducer, actionCreators, actionCreators2, constants }; 
