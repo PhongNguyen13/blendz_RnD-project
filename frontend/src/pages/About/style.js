23731d717d13fbd9774333df76bae3fad85a4528
Jason-5-37 <2207324024@qq.com>
2022-04-26 15:08:48 +1200

    merge

---
+++ frontend/src/pages/About/style.js
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+import { mobile } from "../../responsive";
+
+export const AboutWrapper = styled.div`
+
+
+    // padding-top: 130px;
+    // padding-right: 300px;
+    // padding-left: 300px;
+    padding: 100px 100px;
+
+    ${mobile({ padding: "10px 0px" })}
+
+`;
+
+export const InfoWrapper = styled.div`
+    height: 300px;
+    width: 700px;
+    margin: auto;
+    border-radius: 35px;
+    box-shadow: 12px 12px 2px 1px rgba(192, 109, 31, 0.2);
+    background: rgba(255, 212, 22, 0.2);
+`;
+
+
+
+export const Title = styled.h1`
+    padding-top: 70px;
+    text-align: center;
+    font-size: 30px;
+`;
+
+export const Content = styled.p`
+    padding-top: 30px;
+    padding-left: 30px;
+    padding-right: 30px;
+    text-align: left;
+    font-size: 10px;
+`;
+
+// export const AboutWrapper = styled.div`
+//     // z-index: -1;
+//     // position: absolute;
+//     top: 0;
+//     bottom: 0;
+//     left: 0;
+//     right: 0;
+//     padding-top: 130px;
+//     padding-right: 300px;
+//     padding-left: 300px;
+
+// `;
+
+// export const InfoWrapper = styled.div`
+//     height: 300px;
+//     width: 700px;
+//     margin: auto;
+//     border-radius: 35px;
+//     box-shadow: 12px 12px 2px 1px rgba(192, 109, 31, 0.2);
+//     background: rgba(255, 212, 22, 0.2);
+
+// `;
+
+
+
+// export const Title = styled.h1`
+//     padding-top: 70px;
+//     text-align: center;
+//     font-size: 30px;
+// `;
+
+// export const Content = styled.p`
+//     padding-top: 30px;
+//     padding-left: 30px;
+//     padding-right: 30px;
+//     text-align: left;
+//     font-size: 10px;
+// `;
